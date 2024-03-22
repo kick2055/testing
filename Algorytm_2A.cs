32980b70569ee58ec4fdd008bd512e5f8126659a
kick2055 <56296334+kick2055@users.noreply.github.com>
2024-03-22 11:29:23 +0100

    Add files via upload

---
+++ Algorytm_2A.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bsk_szyfrowanie
+{
+    internal class Algorytm_2A
+    {
+    }
+}
